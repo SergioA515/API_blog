e2316f3e9da14a44d96f2ad4f6c76470a30e7345
Sergio Alejandro <alejoparra191@gmail.com>
2023-02-27 00:40:28 -0500

    Ultimos cambios de prueba

---
+++ src/services/comentarios.servicio.ts
@@ -0,0 +1,81 @@
+/**
+ * @author Sergio Alejadro Parra
+ */
+import { Comentarios } from "@prisma/client";
+import { db } from "../config/db.server";
+// Operaciones CRUD / Listar entidad de la base de datos 
+export const listComentarios = async(): Promise<Comentarios[]> => {
+    return db.comentarios.findMany({
+        select: {
+            id_comentarios: true,
+            contenido_comentarios: true,
+            fecha_comentarios: true,
+            estado_comentarios: true,
+            imagen_comentarios: true,
+            usuario_comentarios_fk: true,
+            publicacion_comentarios_fk: true,
+        }
+    });
+};
+// Obtener un comentario de la base de datos
+export const getComentario = async(id_comentarios: number): Promise<Comentarios | null> => {
+    return db.comentarios.findUnique({
+        where: {
+            id_comentarios: id_comentarios
+        }
+    });
+};
+// Crear un comentario 
+export const createComentario = async(
+    Comentario: Omit<Comentarios, 'id_comentarios, usuario_comentarios_fk, publicacion_comentarios_fk'>
+    ): Promise<Comentarios> => {
+        const {
+            contenido_comentarios,
+            fecha_comentarios,
+            estado_comentarios,
+            imagen_comentarios,
+        } = Comentario;
+    return db.comentarios.create({
+        data: {
+            contenido_comentarios,
+            fecha_comentarios: new Date(fecha_comentarios),
+            estado_comentarios,
+            imagen_comentarios,
+            usuario_comentarios_fk: Comentario.usuario_comentarios_fk,
+            publicacion_comentarios_fk: Comentario.publicacion_comentarios_fk,
+        },
+        select: {
+            id_comentarios: true,
+            contenido_comentarios: true,
+            fecha_comentarios: true,
+            estado_comentarios: true,
+            imagen_comentarios: true,
+            usuario_comentarios_fk: true,
+            publicacion_comentarios_fk: true,
+        },
+    });
+};
+// Actualizar un comentario
+export const updateComentario = async(
+    id_comentarios: number,
+    Comentario: Omit<Comentarios, 'id_comentarios, usuario_comentarios_fk, publicacion_comentarios_fk'>
+): Promise<Comentarios> => {
+    const {
+        contenido_comentarios,
+        fecha_comentarios,
+        estado_comentarios,
+        imagen_comentarios,
+    } = Comentario;
+    return db.comentarios.update({
+        where: {
+            id_comentarios,
+        },
+        data: {
+            contenido_comentarios,
+            fecha_comentarios: new Date(fecha_comentarios),
+            estado_comentarios,
+            imagen_comentarios,
+            usuario_comentarios_fk: Comentario.usuario_comentarios_fk,
+        },
+    });
+};
--- src/services/usuario.servicio.ts
+++ src/services/usuario.servicio.ts
@@ -41,4 +41,89 @@ export const getUsuario = async (NIP_usuarios:number): Promise<Usuarios | null> 
         }
     });
 }
+// Crear las usuarios
+export const createUsuario = async (
+    Usuario: Omit<Usuarios, "NIP_usuarios"> 
+): Promise<Usuarios> => {
+    const {
+        nombre_usuarios, 
+        apellido_usuarios,
+        email_usuarios,
+        genero_usuarios,
+        sede_usuarios,
+        rol_usuarios,
+        estado_usuarios,
+        pass_usuarios,
+        fecha_usuarios,
+        imagen_usuarios
+    } = Usuario;
+    return db.usuarios.create({
+        data: {      
+            nombre_usuarios, 
+            apellido_usuarios,
+            email_usuarios,
+            genero_usuarios,
+            sede_usuarios,
+            rol_usuarios,
+            estado_usuarios,
+            pass_usuarios,
+            fecha_usuarios: new Date(fecha_usuarios),
+            imagen_usuarios,
+        },
+        select:{
+            NIP_usuarios: true,
+            nombre_usuarios: true,
+            apellido_usuarios: true,
+            email_usuarios: true,
+            genero_usuarios: true,
+            sede_usuarios: true,
+            rol_usuarios: true,
+            estado_usuarios: true,
+            pass_usuarios: true,
+            fecha_usuarios: true,
+            imagen_usuarios: true,
+        },
+    });
+};
+export const updateUsuario = async (
+    NIP_usuarios:number,
+    Usuario: Omit<Usuarios, "NIP_usuarios"> 
+): Promise<Usuarios> => {
+    const {
+        nombre_usuarios, 
+        apellido_usuarios,
+        email_usuarios,
+        genero_usuarios,
+        sede_usuarios,
+        rol_usuarios,
+        estado_usuarios,
+        pass_usuarios,
+        fecha_usuarios,
+        imagen_usuarios
+    } = Usuario;
+    return db.usuarios.update({
+        where: {
+            NIP_usuarios,
+        },
+        data: {
+            nombre_usuarios, 
+            apellido_usuarios,
+            email_usuarios,
+            genero_usuarios,
+            sede_usuarios,
+            rol_usuarios,
+            estado_usuarios,
+            pass_usuarios,
+            fecha_usuarios,
+            imagen_usuarios
+        }
+    });
+}
 
+export const deleteUsuario = async (NIP_usuarios:number): Promise<void> => {
+    await db.usuarios.delete({
+        where: {
+            NIP_usuarios,
+        },
+    });
+}
